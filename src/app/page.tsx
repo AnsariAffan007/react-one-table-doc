6dcbaa24e61f4f7ce4707a7f4f9e422491277a5b
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-05 15:00:53 +0530

    add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,8 +9,20 @@ export default function Home() {
           <h1 className="text-6xl font-extrabold">React One Table</h1>
           <h4 className="text-2xl font-bold">Tanstack, but Opiniated</h4>
           <div className="flex gap-x-2 items-stretch justify-center mt-2">
-            <Button variant={"outline"}>Read the Docs</Button>
-            <Button variant={"destructive"}>See Examples</Button>
+            <Button
+              variant={"outline"}
+              className="cursor-pointer"
+              asChild
+            >
+              <Link href={"/docs"}>
+                Read the Docs
+              </Link>
+            </Button>
+            <Button
+              variant={"destructive"}
+              className="cursor-pointer">
+              See Examples
+            </Button>
           </div>
         </div>
       </div>
