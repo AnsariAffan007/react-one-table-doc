159bea791dc027ef07599aaaa0131f65c183f6a2
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-05 14:38:33 +0530

    home page + shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 
 export default function Home() {
   return (
-    <div>Hey</div>
+    <section id="home">
+      <div className="h-[100vh] flex justify-center items-center">
+        <div className="flex flex-col gap-y-2 text-center">
+          <h1 className="text-6xl font-extrabold">React One Table</h1>
+          <h4 className="text-2xl font-semibold">Tanstack table, but Opiniated</h4>
+          <div className="flex gap-x-2 items-stretch">
+            <button>Read the Docs</button>
+            <button>See Examples</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
