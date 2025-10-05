af2c4084e2b9c5bb1fcba02d72d696c4b6a503b2
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-05 22:54:38 +0530

    sidebar menu items creation & mapping

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,67 +1,158 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, ChevronDown, Home, Inbox, Search, Settings } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible"
+import { ScrollArea } from "./ui/scroll-area"
+
+/*
+  Getting Started
+    intro
+    installation
+    quick start
+  Prerequisites
+    Tanstack table overview
+    Column Def
+  Core Concepts
+    Table Component
+    Styling System
+    Functionality Injection
+  Styling
+    Table
+    TableHead
+    TableBody
+    TableHeadRow
+    TableBodyRow
+    TableHeadCell
+    TableBodyCell
+  Functionalities
+    Column Ordering
+    Column Pinning
+    Row Selection
+    Row Reordering
+    Inline Editing
+    Sorting
+    Pagination
+*/
 
 // Menu items.
-const items = [
+const menu = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: "Getting Started",
+    items: [
+      { title: "Introduction", url: "#" },
+      { title: "Installation", url: "#" },
+      { title: "Quick Start", url: "#" },
+    ]
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: "Pre-requisites",
+    items: [
+      { title: "Tanstack Table", url: "#" },
+      { title: "Column Def Type", url: "#" },
+    ]
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: "Core Concepts",
+    items: [
+      { title: "Table Component", url: "#" },
+      { title: "Styling Methods", url: "#" },
+      { title: "Functionality Injection", url: "#" },
+    ]
   },
   {
-    title: "Search",
-    url: "#",
-    icon: Search,
+    title: "Styling",
+    items: [
+      { title: "Table", url: "#" },
+      { title: "TableHead", url: "#" },
+      { title: "TableBody", url: "#" },
+      { title: "TableHeadRow", url: "#" },
+      { title: "TableBodyRow", url: "#" },
+      { title: "TableHeadCell", url: "#" },
+      { title: "TableBodyCell", url: "#" },
+    ]
   },
   {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+    title: "Functionalities",
+    items: [
+      { title: "Column Ordering", url: "#" },
+      { title: "Row Selection", url: "#" },
+      { title: "Row Reordering", url: "#" },
+      { title: "Inline Editing", url: "#" },
+      { title: "Sorting", url: "#" },
+      { title: "Pagination", url: "#" },
+    ]
+  }
 ]
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+
+      <SidebarHeader className="px-4">
+        <h4 className="font-bold">
+          <span>React</span>
+          <span className="text-chart-3">One</span>
+          <span>Table</span>
+        </h4>
+      </SidebarHeader>
+
+      <ScrollArea className='h-[100%]'>
+        <SidebarContent className="pb-40 gap-0">
+
+          {menu.map((menuItem, index) => (
+            <Collapsible key={index} defaultOpen>
+              <SidebarGroup className="py-2">
+
+                <SidebarGroupLabel asChild className="cursor-pointer">
+                  <CollapsibleTrigger>
+                    {menuItem.title}
+                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+
+                <CollapsibleContent>
+                  <SidebarGroupContent>
+
+                    <SidebarMenu>
+                      {menuItem.items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                          <SidebarMenuButton asChild>
+                            <a href={item.url}>
+                              {/* <item.icon /> */}
+                              <span>{item.title}</span>
+                            </a>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+
+                  </SidebarGroupContent>
+                </CollapsibleContent>
+
+              </SidebarGroup>
+            </Collapsible>
+          ))}
+
+        </SidebarContent>
+      </ScrollArea>
+
+      {/* <SidebarFooter className="px-4">
+        <h4 className="text-primary">
+          Version 1.0.0
+        </h4>
+      </SidebarFooter> */}
+
     </Sidebar>
   )
 }
