83fa5bab8dbe664d38d00764907204550dad4787
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-05 14:43:57 +0530

    home page -- improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -5,10 +6,10 @@ export default function Home() {
       <div className="h-[100vh] flex justify-center items-center">
         <div className="flex flex-col gap-y-2 text-center">
           <h1 className="text-6xl font-extrabold">React One Table</h1>
-          <h4 className="text-2xl font-semibold">Tanstack table, but Opiniated</h4>
-          <div className="flex gap-x-2 items-stretch">
-            <button>Read the Docs</button>
-            <button>See Examples</button>
+          <h4 className="text-2xl font-bold">Tanstack, but Opiniated</h4>
+          <div className="flex gap-x-2 items-stretch justify-center mt-2">
+            <Button variant={"outline"}>Read the Docs</Button>
+            <Button variant={"destructive"}>See Examples</Button>
           </div>
         </div>
       </div>
